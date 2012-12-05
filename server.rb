12aa26e1cce14590debca1b4195a5eaba6673892
Peter Wu <peter.wu@xbridge.com>
2012-12-05 12:44:49 +0000

    slightly improved using more restful style

--- server.rb
+++ server.rb
@@ -5,29 +5,31 @@ require 'base64'
 
 require_relative 'helpers/server_helper'
 
-
 # .find(:all)
 get '/files.json' do
-  json(Dir["#{server_path}/**/*"].select{|v| File.file?(v)}.map{|v| {version: 1, path: v.gsub(/^#{server_path}/, "")}}, :encoder => :to_json, :content_type => :js)
+  json(Dir["#{server_path}/**/*"].select{|v| File.file?(v)}.map{|v| {last_update: File.mtime(v), path: v.gsub(/^#{server_path}\//, "")}}, :encoder => :to_json, :content_type => :js)
 end
 
-# .download "/xxx/xxx/xxx.xx"
-get '/files/download.json' do
-  if params[:path]
-    path = File.join(server_path, params[:path])
-    if path && File.exist?(path)
-      file = nil
-      File.open(path, 'rb'){|f| file = f.read}
+# .find(:path).get(:download)
+get %r{^/files/(.*)/download\.json$} do |path|
+  puts "****", path
+  file_path = File.join(server_path, path)
+  if file_path && File.exist?(file_path)
+    file = nil
+    File.open(file_path, 'rb'){|f| file = f.read}
 
-      json({path: params["path"], file: Base64.encode64(file)}, :encoder => :to_json, :content_type => :js)
-    end
+    json({path: path, file: Base64.encode64(file)}, :encoder => :to_json, :content_type => :js)
   end
 end
 
-
-get '/files/:path' do |path|
-  raise "Unauthorised"
+#.find(:path)
+get %r{^/files/(.*)\.json$} do |path|
+  file_path = File.join(server_path, path)
+  if file_path && File.exist?(file_path)
+    json({path: path, last_update: File.mtime(file_path)}, :encoder => :to_json, :content_type => :js)
+  end
 end
+
 
 get '/*' do
   raise "Unauthorised"
