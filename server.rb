337ff911b608717297fda98753ec87a7e1f86895
Peter Wu <peter.wu@xbridge.com>
2012-12-07 10:38:41 +0000

    now the model is call server_file

--- server.rb
+++ server.rb
@@ -6,12 +6,12 @@ require 'base64'
 require_relative 'helpers/server_helper'
 
 # .find(:all)
-get '/files.json' do
+get '/server_files.json' do
   json(Dir["#{server_path}/**/*"].select{|v| File.file?(v)}.map{|v| {last_update: File.mtime(v), path: v.gsub(/^#{server_path}\//, "")}}, :encoder => :to_json, :content_type => :js)
 end
 
 # .find(:path).get(:download)
-get %r{^/files/(.*)/download\.json$} do |path|
+get %r{^/server_files/(.*)/download\.json$} do |path|
   file_path = File.join(server_path, path)
   if file_path && File.exist?(file_path)
     file = nil
@@ -24,7 +24,7 @@ get %r{^/files/(.*)/download\.json$} do |path|
 end
 
 #.find(:path)
-get %r{^/files/(.*)\.json$} do |path|
+get %r{^/server_files/(.*)\.json$} do |path|
   file_path = File.join(server_path, path)
   if file_path && File.exist?(file_path)
     json({path: path, last_update: File.mtime(file_path)}, :encoder => :to_json, :content_type => :js)
