20f11bfcad41b7cfc8c2de011f785076709191e7
Peter Wu <peter.wu@xbridge.com>
2012-12-08 14:42:22 -0800

    fixed spacing

--- helpers/server_helper.rb
+++ helpers/server_helper.rb
@@ -1,7 +1,8 @@
+require 'yaml'
 helpers do
 
-	def server_path
-		@@server_path ||= YAML.load(File.read(File.join(Dir.pwd, "config/setup.yml")))["server_path"]
-	end
+  def server_path
+    @@server_path ||= YAML.load(File.read(File.join(Dir.pwd, "config/setup.yml")))["server_path"]
+  end
 
 end
