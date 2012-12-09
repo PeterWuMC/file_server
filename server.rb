5658541310742f38a2177d03976f405fb6bb4ad4
Peter Wu <peter.wu@xbridge.com>
2012-12-09 00:38:15 -0800

    encode the file path in the url with base64

--- server.rb
+++ server.rb
@@ -9,29 +9,33 @@ require_relative 'helpers/server_helper'
 
 # .find(:all)
 get '/server_files.json' do
-  json(Dir["#{server_path}/**/*"].select{|v| File.file?(v)}.map{|v| {last_update: File.mtime(v), path: v.gsub(/^#{server_path}\//, "")}}, :encoder => :to_json, :content_type => :js)
+  json(Dir["#{server_path}/**/*"].select{|v| File.file?(v)}.map{|v| {
+    last_update: File.mtime(v),
+    path:        v.gsub!(/^#{server_path}\//, ""),
+    key:         Base64.encode64(v)
+  }}, :encoder => :to_json, :content_type => :js)
 end
 
 # .find(:path).get(:download)
-get %r{^/server_files/(.*)/download\.json$} do |path|
-  path.gsub!("%20", " ")
+get %r{/server_files/(.*)/download\.json} do |key|
+  path = Base64.decode64 key
   file_path = File.join(server_path, path)
   if file_path && File.exist?(file_path)
     file = nil
     File.open(file_path, 'rb'){|f| file = f.read}
 
-    json({path: path, file_content: Base64.encode64(file)}, :encoder => :to_json, :content_type => :js)
+    json({key: key, path: path, file_content: Base64.encode64(file)}, :encoder => :to_json, :content_type => :js)
   else
     raise Sinatra::NotFound
   end
 end
 
 #.find(:path)
-get %r{^/server_files/(.*)\.json$} do |path|
-  path.gsub!("%20", " ")
+get %r{/server_files/(.*)\.json} do |key|
+  path = Base64.decode64 key
   file_path = File.join(server_path, path)
   if file_path && File.exist?(file_path)
-    json({path: path, last_update: File.mtime(file_path)}, :encoder => :to_json, :content_type => :js)
+    json({key: key, path: path, last_update: File.mtime(file_path)}, :encoder => :to_json, :content_type => :js)
   else
     raise Sinatra::NotFound
   end
