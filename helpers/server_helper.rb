9b6fe94738d6b54b803ea43c1f8e2d7b467697a7
Peter Wu <peter.wu@xbridge.com>
2012-12-10 14:49:41 +0000

    added update, delete and new functions

--- helpers/server_helper.rb
+++ helpers/server_helper.rb
@@ -2,13 +2,33 @@ require 'yaml'
 helpers do
 
   def server_path
-    @@server_path ||= YAML.load(File.read(File.join(Dir.pwd, "config/setup.yml")))["server_path"]
+    @server_path ||= YAML.load(File.read(File.join(Dir.pwd, "config/setup.yml")))["server_path"]
   end
 
-  def decode_key key
-    Base64.strict_decode64 key
+  def check_and_return_path key
+  	path      = Base64.strict_decode64(key)
+    full_path = File.join(server_path, path)
+    raise Sinatra::NotFound if !(full_path && File.exist?(full_path))
+    return full_path, path
   rescue
     raise Sinatra::NotFound
+  end
+
+
+
+
+  # TODO: get this from file_manager
+  def self.create_folders_for full_path
+    full_path = File.dirname(full_path)
+
+    return if File.directory?(full_path) || ["/", "."].include?(full_path)
+    create_folders_for(full_path)
+    Dir::mkdir(full_path)
+  end
+
+  def self.write_file full_path, file_content
+    create_folders_for full_path
+    File.open(full_path, 'w'){|f| f.write(file_content)}
   end
 
 end
--- server.rb
+++ server.rb
@@ -16,32 +16,57 @@ get '/server_files.json' do
   }}, :encoder => :to_json, :content_type => :js)
 end
 
-# .find(:path).get(:download)
+# .find(:key).get(:download)
 get %r{^/server_files/(.*)/download\.json$} do |key|
-  path = decode_key key
-  file_path = File.join(server_path, path)
-  if file_path && File.exist?(file_path)
-    file = nil
-    File.open(file_path, 'rb'){|f| file = f.read}
+  full_path, path = check_and_return_path(key)
 
-    json({key: key, path: path, file_content: Base64.encode64(file)}, :encoder => :to_json, :content_type => :js)
-  else
-    raise Sinatra::NotFound
-  end
+  file = nil
+  File.open(full_path, 'rb'){|f| file = f.read}
+
+  json({key: key, path: path, file_content: Base64.encode64(file)}, :encoder => :to_json, :content_type => :js)
 end
 
-#.find(:path)
+#.find(:key)
 get %r{^/server_files/(.*)\.json$} do |key|
-  path = decode_key key
-  file_path = File.join(server_path, path)
-  if file_path && File.exist?(file_path)
-    json({key: key, path: path, last_update: File.mtime(file_path)}, :encoder => :to_json, :content_type => :js)
-  else
-    raise Sinatra::NotFound
-  end
+  full_path, path = check_and_return_path(key)
+
+  json({key: key, path: path, last_update: File.mtime(full_path)}, :encoder => :to_json, :content_type => :js)
+end
+
+#.find(:key).update_attributes(:xxxx, yyyy)
+put %r{^/server_files/(.*)\.json$} do |key|
+  full_path, path = check_and_return_path(key)
+
+  server_file  = JSON.parse(request.body.read)["server_file"]
+
+  path         = server_file["path"]
+  file_content = Base64.decode64(server_file["file_content"])
+  write_file(path, file_content)
+
+  status 200
+end
+
+#.delete(:key)
+delete %r{^/server_files/(.*)\.json$} do |key|
+  full_path, path = check_and_return_path(key)
+  File.delete(full_path)
+
+  status 200
+end
+
+#.new(:xxx => yyy)
+post '/server_files/new.json' do
+  server_file  = JSON.parse(request.body.read)["server_file"]
+
+  path         = server_file["path"]
+  file_content = Base64.decode64(server_file["file_content"])
+  write_file(path, file_content)
+
+  status 200
 end
 
 
 get '/*' do
   raise "Unauthorised"
 end
+
