b95a4acb1bdee74336cd886ca49a25fc8a098b9a
Peter Wu <peter.wu@xbridge.com>
2013-01-04 18:23:36 +0000

    path will not exclude the file name

--- models/shared_file.rb
+++ models/shared_file.rb
@@ -3,11 +3,11 @@ class SharedFile
   attr_reader :type, :name, :path, :key
 
   def initialize file
-    @file        = file
     @type        = File.file?(file) ? "file" : "folder"
     @name        = File.basename(file)
-    @path        = file.gsub(/^#{server_path}\//, "")
-    @key         = Base64.strict_encode64(@path)
+    temp_path    = file.gsub(/^#{server_path}/, "")
+    @path        = File.dirname(temp_path)
+    @key         = Base64.strict_encode64(temp_path)
     if @type == "file"
       @last_update = File.mtime(file).utc
       @size        = File.size(file)
