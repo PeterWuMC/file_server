ab554fbec9f07a22242039d7f6f81a6b80d44b56
Peter Wu <peter.wu@xbridge.com>
2013-01-15 14:12:38 +0000

    moved around the controller steps

--- routes/routes.rb
+++ routes/routes.rb
@@ -1,2 +1,4 @@
 require_relative 'registration'
 require_relative 'password_reset'
+require_relative 'server_files'
+require_relative 'server_folders'
---
+++ routes/server_files.rb
@@ -0,0 +1,69 @@
+class WuFileServer < Sinatra::Application
+
+  #.delete(:key)
+    delete %r{^/projects/[^/]*/server_files/[^/]*.json$} do
+      if File.exists? && File.file?(@full_path)
+        File.delete(@full_path)
+        status 200
+      else
+        halt 404
+      end
+    end
+
+    get %r{^/projects/[^/]*/server_files/[^/]*/download$} do
+      send_file(@full_path, :disposition => 'attachment', :filename => File.basename(@full_path))
+      status 200
+    end
+
+
+    # # .find(:all)
+    # get %r{/project/(.*)/server_files.json} do |project_key|
+    #   json(Dir["#{server_path}/**/*"].select{|v| File.file?(v)}.map{|v|
+    #     SharedFile.new(v)
+    #   }, :encoder => :to_json, :content_type => :js)
+    # end
+
+    # # .find(:key).get(:download)
+    # get %r{^/server_files/(.*)/download\.json$} do |key|
+    #   full_path, path = check_and_return_path(key)
+
+    #   file = nil
+    #   File.open(full_path, 'rb'){|f| file = f.read}
+
+    # # TODO
+    #   json({key: key, path: path, file_content: Base64.encode64(file)}, :encoder => :to_json, :content_type => :js)
+    # end
+
+    # #.find(:key)
+    # get %r{^/server_files/(.*)\.json$} do |key|
+    #   full_path, path = check_and_return_path(key)
+
+    #   json(SharedFile.new(full_path), :encoder => :to_json, :content_type => :js)
+    # end
+
+    # #.find(:key).update_attributes(:xxxx, yyyy)
+    # put %r{^/server_files/(.*)\.json$} do |key|
+    #   full_path, path = check_and_return_path(key)
+
+    #   server_file  = JSON.parse(request.body.read)["server_file"]
+
+    #   path         = server_file["path"]
+    #   file_content = Base64.decode64(server_file["file_content"])
+    #   write_file(full_path, file_content)
+
+    #   status 200
+    # end
+
+    # #.new(:xxx => yyy)
+    # post '/server_files.json' do
+    #   server_file  = JSON.parse(request.body.read)["server_file"]
+
+    #   path         = server_file["path"]
+    #   full_path    = File.join(server_path, path)
+    #   file_content = Base64.decode64(server_file["file_content"])
+    #   write_file(full_path, file_content)
+
+    #   status 200
+    # end
+
+  end
