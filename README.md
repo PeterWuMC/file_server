894634b714e4372bfa3ed625cd45f8d1f2247ba9
Peter Wu <peter.wu@xbridge.com>
2013-01-17 17:02:55 +0000

    added read me and fixed formatting

---
+++ README.md
@@ -0,0 +1,8 @@
+To install RMagick on ImageMagick 6.8 here is the steps
+
+```
+cd /usr/local/Cellar/imagemagick/6.8.0-10/lib
+ln -s libMagick++-Q16.7.dylib   libMagick++.dylib
+ln -s libMagickCore-Q16.7.dylib libMagickCore.dylib
+ln -s libMagickWand-Q16.7.dylib libMagickWand.dylib
+```
--- routes/server_folders.rb
+++ routes/server_folders.rb
@@ -1,35 +1,35 @@
 class WuFileServer < Sinatra::Application
 
-	get %r{/projects/list.json} do
-	    json(@user.projects.all.map(&:to_h), :encoder => :to_json, :content_type => :js)
-	  end
+  get %r{/projects/list.json} do
+    json(@user.projects.all.map(&:to_h), :encoder => :to_json, :content_type => :js)
+  end
 
-	  get %r{^/projects/[^/]*/server_folders/[^/]*/list.json$} do
-	    json(Dir["#{@full_path}/*"].map{|v|
-	      SharedFile.new(v, @project)
-	    }.sort_by{|v| v.type}.reverse!, :encoder => :to_json, :content_type => :js)
-	  end
+  get %r{^/projects/[^/]*/server_folders/[^/]*/list.json$} do
+    json(Dir["#{@full_path}/*"].map{|v|
+      SharedFile.new(v, @project)
+    }.sort_by{|v| v.type}.reverse!, :encoder => :to_json, :content_type => :js)
+  end
 
-	  post %r{^/projects/[^/]*/server_folders/[^/]*.json$} do
-	    folder_name = params["name"]
-	    create_folders_for File.join(@full_path, "#{folder_name}/tmp")
-	    status 200
-	  end
+  post %r{^/projects/[^/]*/server_folders/[^/]*.json$} do
+    folder_name = params["name"]
+    create_folders_for File.join(@full_path, "#{folder_name}/tmp")
+    status 200
+  end
 
-	  post %r{^/projects/[^/]*/server_folders/[^/]*/upload$} do
-	    halt 500 if !params['file']
-	    write_file(File.join(@full_path, params['file'][:filename]), params['file'][:tempfile].read)
+  post %r{^/projects/[^/]*/server_folders/[^/]*/upload$} do
+    halt 500 if !params['file']
+    write_file(File.join(@full_path, params['file'][:filename]), params['file'][:tempfile].read)
 
-	    json(SharedFile.new(File.join(@full_path, params['file'][:filename]), @project), :encoder => :to_json, :content_type => :js)
-	  end
+    json(SharedFile.new(File.join(@full_path, params['file'][:filename]), @project), :encoder => :to_json, :content_type => :js)
+  end
 
-	  delete %r{^/projects/[^/]*/server_folders/[^/]*.json$} do
-	    if File.exists? && File.directory?(@full_path)
-	      FileUtils.rm_rf(@full_path)
-	      status 200
-	    else
-	      halt 404
-	    end
-	  end
+  delete %r{^/projects/[^/]*/server_folders/[^/]*.json$} do
+    if File.exists? && File.directory?(@full_path)
+      FileUtils.rm_rf(@full_path)
+      status 200
+    else
+      halt 404
+    end
+  end
 
 end
